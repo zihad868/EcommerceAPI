63897dc82ddb32a4ef5f81c1ab6f1ac4af04480e
zihad868 <zihadh868@gmail.com>
2024-07-01 17:40:47 +0600

    Create Category Models

--- product/models.py
+++ product/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(unique=True)
+    featured = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['title']
+        
+    def __str__(self):
+        return self.title
